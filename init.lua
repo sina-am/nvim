ee19a8a30f675a4a524a62e08b58ce881abfbd1c
Sina Aarabi <sinaaarabi2@gmail.com>
2024-02-29 16:00:19 +0330

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,20 +1,32 @@
 require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'	-- Package manager
-    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-    use 'morhetz/gruvbox'         -- Dear gruvebox
+    use 'wbthomason/packer.nvim'		-- Package manager
+    use 'neovim/nvim-lspconfig' 		-- Configurations for Nvim LSP
+    use 'morhetz/gruvbox'         		-- Dear gruvebox
 
-    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'hrsh7th/nvim-cmp' 			-- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' 			-- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' 		-- Snippets source for nvim-cmp
+    use 'scrooloose/nerdcommenter'		-- Commenter
 
-    use {'akinsho/toggleterm.nvim', tag = '*'}
+    use {'akinsho/toggleterm.nvim', tag = '*'}	-- Terminal
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    use {					-- Searching files
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-	use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use 'L3MON4D3/LuaSnip' 			-- Snippets plugin
+    use {
+        'nvim-tree/nvim-tree.lua',		-- File explorer tree
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    }
+    use "astral-sh/ruff-lsp"			-- Ruff LSP for python
 end)
+
+-- disable netrw 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 -- GENERAL SETTINGS 
 vim.cmd('colorscheme gruvbox')
@@ -23,12 +35,14 @@ vim.cmd('set encoding=utf-8')
 vim.cmd('set termguicolors')
 vim.cmd('colorscheme gruvbox')
 vim.cmd('set background=dark')
+vim.cmd('set relativenumber')
 vim.cmd('set number')
 vim.cmd('set bs=2')
 vim.cmd('set tabstop=4')
 vim.cmd('set shiftwidth=4')
 vim.cmd('set expandtab')
 vim.cmd('set noswapfile')
+
 vim.cmd('set hlsearch')
 vim.cmd('set incsearch')
 vim.cmd('set ignorecase')
@@ -37,14 +51,74 @@ vim.cmd('set autoindent')
 vim.cmd('set hidden')
 vim.cmd('set splitbelow')
 
--- Setup language servers.
+vim.cmd("tnoremap <Esc> <C-\\><C-n>")
 
+-- Setup language servers.
 local lspconfig = require('lspconfig')
 util = require "lspconfig/util"
--- Python support
-lspconfig.pyright.setup {}
 
-lspconfig.tsserver.setup {}
+-- Python support
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gg', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+-- Configure `ruff-lsp`.
+require('lspconfig').ruff_lsp.setup {
+  on_attach = on_attach,
+  init_options = {
+    settings = {
+      args = {},
+    }
+  }
+}
+--[[lspconfig.pyright.setup {]]
+    --[[cmd = { "pyright-langserver", "--stdio" },]]
+    --[[filetypes = { "python" },]]
+    --[[root_dir = function(filename)]]
+          --[[return util.root_pattern(unpack(root_files))(filename) or util.path.dirname(filename) or util.root_pattern(".venv", ".git")]]
+        --[[end,]]
+    --[[settings = {]]
+      --[[python = {]]
+        --[[analysis = {]]
+          --[[autoSearchPaths = true,]]
+          --[[diagnosticMode = "workspace",]]
+          --[[useLibraryCodeForTypes = true,]]
+          --[[disableOrganizeImports = false,]]
+          --[[typeCheckingMode = "strict", -- "basic", "off"]]
+        --[[},]]
+      --[[},]]
+    --[[},]]
+--[[}]]
+
+-- lspconfig.tsserver.setup {}
 
 -- Golang support
 lspconfig.gopls.setup {
@@ -61,34 +135,26 @@ lspconfig.gopls.setup {
     },
   }
 
+
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.go',
   callback = function()
     vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
   end
 })
-
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
     -- Enable completion triggered by <c-x><c-o>
     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gg', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
@@ -132,9 +198,6 @@ cmp.setup {
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4), -- Up
-    ['<C-f>'] = cmp.mapping.scroll_docs(4), -- Down
-    -- C-b (back) C-f (forward) for snippet placeholder navigation.
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
@@ -165,8 +228,6 @@ cmp.setup {
   },
 }
 
-
-
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -175,4 +236,20 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 require("toggleterm").setup{
     open_mapping = [[<C-t>]],
+}
+
+-- pass to setup along with your other options
+require("nvim-tree").setup {
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  }
 }
